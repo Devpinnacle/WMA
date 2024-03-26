6d485a026cea739d63b1747bb7cb0f3140c4d555
dev001 <dev@gmail.com>
2024-03-26 14:48:34 +0530

    created login page

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import "./Login.css"
+
+const Login = () => {
+    return (
+        <div className="login-container">
+            <form className='login-form'>
+                <h2 className='title'>LOG IN</h2>
+                    <div className="username-input">
+                        <label htmlFor='username'>Username*</label>
+                        <div className='form-input'>
+                            <input
+                                className='input-box'
+                                type="username"
+                                id="username"
+                                name="username"
+                                required
+                            />
+                        </div>
+                    </div>
+                    <div className="password-input">
+                        <label htmlFor='password'>Password*</label>
+                        <div className='form-input'>
+                            <input
+                                className='input-box'
+                                type="password"
+                                id="password"
+                                name="password"
+                                required
+                            />
+                        </div>
+                    </div>
+                    <div className="flex-center">
+                        <button className="btn-login">
+                            Log in
+                        </button>
+                    </div>
+            </form>
+        </div>
+
+    )
+}
+export default Login
