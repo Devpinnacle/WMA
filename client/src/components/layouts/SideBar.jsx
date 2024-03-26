b4c49af2e09c3065a5f071af4c5cc6609eeab694
dev001 <dev@gmail.com>
2024-03-26 17:20:25 +0530

    created sidebar

--- client/src/components/layouts/SideBar.jsx
+++ client/src/components/layouts/SideBar.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
+import './SideBar.css'
+import dashboardIcon from "../../assets/icons/dashboardWhite.png"
+
 
 const SideBar = () => {
-  return (
-    <div>
-        
+    return (
+        <div className='side-bar'>
+            <div className="profile-section">
 
-    </div>
-  )
+            </div>
+            <div className="profile-line"></div>
+            <div className="navigation-tabs">
+                <span className="icon"><img className='dashboardIcon' src={dashboardIcon} /></span>
+                <span className="nav-content">Dashboard</span>
+            </div>
+            <div className="navigation-tabs">Projects</div>
+            <div className="navigation-tabs">Reports</div>
+            <div className="navigation-tabs">Users</div>
+
+        </div>
+
+    )
 }
 
 export default SideBar
