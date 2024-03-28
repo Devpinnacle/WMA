8b2107359ad7cb5ccda9f90c4818496497fe8cb7
dev001 <dev@gmail.com>
2024-03-28 12:01:57 +0530

    app changes to css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,9 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Login from './pages/Login'
+import SideBar from './components/layouts/sidebar/SideBar'
+import MainLayout from './components/layouts/sidebar/MainLayout'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,7 +13,9 @@ function App() {
 
   return (
     <>
-      <Login/>
+      {/* <Login/> */}
+      {/* <SideBar/> */}
+      <MainLayout/>
     </>
   )
 }
