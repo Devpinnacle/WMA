78a7c48395e2daab70ae315642ab35ecf1e7b2a0
Niharika <dev@gmail.com>
2024-04-03 12:48:12 +0530

    updated view project

---
+++ client/src/components/ui/SelectInput.jsx
@@ -0,0 +1,82 @@
+import ReactSelect from "react-select"
+export default function SelectInput(props) {
+    const height = props.height || "36px";
+    return (
+        <ReactSelect
+            {...props}
+            menuPlacement="auto"
+            components={{
+                IndicatorSeparator: () => null,
+            }}
+            styles={{
+                control: (provided, state) => ({
+                    ...provided,
+                    fontSize: "1.3rem",
+                    borderRadius: "8px",
+                    borderColor: "#bdb8b3",
+                    boxShadow: "none",
+                    "&:hover": {
+                        border: "1px solid ##FBEFDA",
+                    },
+                    minHeight: height,
+                    height,
+                    backgroundColor: "#fff",
+                    cursor: "pointer",
+                    position: "relative",
+                }),
+                option: (provided, state) => ({
+                    ...provided,
+                    fontSize: "1.3rem",
+                    cursor: "pointer",
+
+                    backgroundColor: state.isSelected
+                        ? "#F3CF96"
+                        : state.isFocused
+                            ? "#FBEFDA"
+                            : "#fff",
+                }),
+                menu: (provided) => ({
+                    ...provided,
+                    borderRadius: "8px",
+                    overflow: "hidden",
+                    zIndex: 9999,
+                }),
+                singleValue: (provided, state) => ({
+                    ...provided,
+                    color: state.isDisabled ? "#716c69" : "#20262e",
+                }),
+                
+                dropdownIndicator: (base) => ({
+                    ...base,
+                    color: 'black', 
+                    '& svg': {
+                      fill: 'black', 
+                      scale: '0.8',
+                    },
+                  }),
+                 
+
+                valueContainer: (provided) => ({
+                    ...provided,
+                    height,
+                    padding: "0 0 0 5px",
+                }),
+                input: (provided) => ({
+                    ...provided,
+                    height,
+                    margin: "0px",
+                    padding: "0px",
+                }),
+                indicatorSeparator: () => ({
+                    display: "none",
+                }),
+                indicatorsContainer: (provided) => ({
+                    ...provided,
+                    height,
+
+                }),
+            }}
+        />
+    )
+}
+
--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -2,29 +2,50 @@ import React from 'react'
 import MainContainer from '../components/layouts/sidebar/MainContainer'
 import './Projects.css'
 import Icon from '../components/ui/Icon'
+import SelectInput from '../components/ui/SelectInput'
 
 const Projects = () => {
-    const project = [{title:''}]
+    // const project = [{title:''}]
     return (
         <MainContainer>
-            <div className='project-customize'>
-                <div className='search-box'>
-                    <input
-                        id="keyword"
-                        name="keyword"
-                        type="text"
-                        placeholder="Search for projects"
-                        autoComplete="new-off"
-                    />
-                </div>
-                <Icon
-                    title="Search"
-                    name="search-icon"
-                    size="2rem"
-                />
-            </div>
+            <div className="project-top-container mb-1">
+                <div className="project-top-actions ml-1">
+                    <div className="project-top-actions-left row-gap-1">
+                        <div className="flex-end">
+                            <div className='search-box'>
+                                <input
+                                    id="keyword"
+                                    name="keyword"
+                                    type="text"
+                                    placeholder="Search for projects"
+                                    autoComplete="new-off"
+                                />
+                                <Icon
+                                    title="Search"
+                                    name="search-icon"
+                                    size="2rem"
+                                />
+                            </div>
+                            <div className='project-tags'>
+                                <SelectInput
+                                    placeholder="Tags"
+                                    isSearchable={false}
+                                />
 
-            {/* <div className='project-container'>
+                            </div>
+                        </div>
+                    </div>
+                    <button className="btn-outline" >
+                        <Icon
+                            name="add-outline"
+                            size="2rem"
+                        />
+                        Add Project
+                    </button>
+                </div>
+            </div>
+                  
+            <div className='project-container'>
                 <div className='project-item'>
                     <div className='project-title'>
                         <Icon
@@ -32,7 +53,7 @@ const Projects = () => {
                             name="project-outline"
                             size="2rem"
                         />
-                        Book Better-Hospitality
+                        Book Better
                     </div>
                     <div className='project-info'>
 
@@ -41,8 +62,8 @@ const Projects = () => {
 
                     </div>
                 </div>
-            </div> */}
-            <div className='project-container'>
+            </div>
+            {/* <div className='project-container'>
                 {project.map((project, index) => (
                     <div className='project-item' key={index}>
                         <div className='project-title'>
@@ -54,17 +75,14 @@ const Projects = () => {
                             {project.title}
                         </div>
                         <div className='project-info'>
-                            {/* {project.info} */}
+                            {project.info}
                         </div>
                         <div className='project-tags'>
 
                         </div>
                     </div>
                 ))}
-            </div>
-
-
-
+            </div> */}
         </MainContainer>
     )
 }
