5ab344894e94705ac145755d6f3d2daa45d21063
Rakesh <dev@gmail.com>
2024-04-03 13:34:21 +0530

    minor changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -29,7 +29,7 @@ function App() {
               index: true,
               element: (
                 <ProtectedRoute>
-                  <Projects/>
+                  <DashBoard/>
                 </ProtectedRoute>
               ),
             },
