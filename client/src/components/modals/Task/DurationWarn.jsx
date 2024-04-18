8ce07248badbf644e4c403a8b8188d746f5c4321
Niharika <dev@gmail.com>
2024-04-18 12:01:49 +0530

    user notification modal

--- client/src/components/modals/Task/DurationWarn.jsx
+++ client/src/components/modals/Task/DurationWarn.jsx
@@ -1,14 +1,29 @@
 import React from "react";
 import ModalContainer from "../ModalContainer";
+import "./DurationWarn.css"
 
-const DurationWarn = ({head, yes, no}) => {
+const DurationWarn = ({ head, yes, no }) => {
   return (
     <ModalContainer onCancel={no} backdropClass={"backdrop-dark"}>
-      <div className="modal-container modal-centered user-modal">
+      <div className="modal-container modal-centered user-modal duration-warn-modal">
         <h1 style={{ color: "black" }}>{head}</h1>
-        <p style={{ color: "black" }}>Duration is not updated, is it OK</p>
-        <button style={{ color: "black" }} onClick={yes}>yes</button>
-        <button style={{ color: "black" }} onClick={no}>no</button>
+        <div className="modal-header">
+          <div className='title-container'>
+            <span className='title' style={{ color: "#3D405B", fontWeight: "700", fontSize: "57px" }}>{head}</span>
+          </div>
+        </div>
+        <div className="del-content ml-5">Duration is not updated. Do you want to update the duration?</div>
+        <div className="button-container p-1 ml-3" style={{ display: "flex" }}>
+          {/* <button style={{ color: "black" }} onClick={yes}>yes</button>
+          <button style={{ color: "black" }} onClick={no}>no</button> */}
+          <button
+            className="btn-outline" onClick={yes}>
+            Yes
+          </button>
+          <button className="btn-del" onClick={no}>
+            No
+          </button>
+        </div>
       </div>
     </ModalContainer>
   );
