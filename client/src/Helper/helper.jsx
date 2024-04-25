9582eb8ae7965c4b735d261458113a91655cd305
Niharika <dev@gmail.com>
2024-04-25 11:27:39 +0530

    download csv

--- client/src/Helper/helper.jsx
+++ client/src/Helper/helper.jsx
@@ -16,5 +16,5 @@ export const minutesToHoursAndMinutes = (totalMinutes) => {
     const year = date.getFullYear();
     const month = (date.getMonth() + 1).toString().padStart(2, '0');
     const day = date.getDate().toString().padStart(2, '0');
-    return `${year}-${month}-${day}`.toString();
+    return `${day}-${month}-${year}`.toString();
 }
